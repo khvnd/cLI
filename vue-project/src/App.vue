c5679f3e56b5c204a293ae1171b34c6bf3a463f3
Nadezhda Kharitonova <blackwolf9283@gmail.com>
2025-02-07 22:11:56 +0600

    app

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  <div >
+    <MainHeader />
+    <router-view />
+    <MainFooter />
+  </div>
+  </template>
+  
+  <script>
+  import MainFooter from '@/components/MainFooter.vue';
+  import MainHeader from '@/components/MainHeader.vue';
+  
+  
+  export default {
+    name: 'App',
+    components: {
+      MainHeader,
+      MainFooter
+      
     }
   }
-}
-</style>
+  </script>
+  
+  <style lang="scss">
+  
+  </style>
